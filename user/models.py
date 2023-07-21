eed4dc0a6927af8effc6b330f19564473bb46fe7
jueun <jeyoo7@naver.com>
2023-07-21 16:41:03 +0900

    user-model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender_choices = [
+        ('F', '여성'),
+        ('M', '남성'),
+    ]
+    gender = models.CharField(max_length=1, choices=gender_choices, null=True, blank=True)
+    is_pregnant = models.BooleanField(default=False)
+    is_breastfeeding = models.BooleanField(default=False)
