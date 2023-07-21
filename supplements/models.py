ff3d3db3c59862d88c7807ceb1300c14e5f7d0ba
jueun <jeyoo7@naver.com>
2023-07-21 18:40:12 +0900

    sup_model 작성

--- supplements/models.py
+++ supplements/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Nutrient(models.Model):
+    name = models.CharField(max_length=200)
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Synonym(models.Model):
+    nutrient = models.ForeignKey(Nutrient, related_name='synonyms', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class NutrientBulk(models.Model):
+    name = models.CharField(max_length=200)
+    nutrients = models.ManyToManyField(Nutrient, through='NutrientDosage')
+
+    def __str__(self):
+        return self.name
+    
+class NutrientDosage(models.Model):
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    nutrientberk = models.ForeignKey(NutrientBulk, on_delete=models.CASCADE)
+    dosage = models.FloatField(default=0.0)
