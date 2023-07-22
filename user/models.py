84870ae9249a4f5f9924d91d3aba2ab24012da60
jueun <jeyoo7@naver.com>
2023-07-22 22:58:45 +0900

    userintake

--- user/models.py
+++ user/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     breastfeeding = models.BooleanField(default=False)
 
     recommended = models.ForeignKey('supplements.RecommendedIntake', on_delete=models.SET_NULL, null=True, related_name='users')
+    intake = models.ManyToManyField('supplements.Nutrient', through='UserIntake')
 
 #사용자의 영양제를 의미함. 영양제 데이터와 사용자의 데이터를 연결해서
 #따로 중간table을 만든 이유는 영양제 이름 때매.. 뭐 저장한 시각도 추가시킬 수 있음
@@ -21,13 +22,10 @@ class UserSupplement(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.supplement.name}'
 
-#사용자가 입력한 영양제들의 총량 데이터.. 이놈을 고쳐야함!!!!!!!!!!!!!!
-class UserTotalIntake(models.Model):
+class UserIntake(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    nutrient = models.ForeignKey('supplements.RecommendedIntake', on_delete=models.CASCADE)
-    total_intake = models.FloatField(default=0.0)  # 누적 섭취량
+    nutrient = models.ForeignKey('supplements.Nutrient', on_delete=models.CASCADE)
+    dosage = models.FloatField(default=0.0)
 
-    def __str__(self):
-        return f'{self.user.username} - {self.total_intake}'
 
 #누적에 대한 정의는 여기 말고 view에서 하기로 해요..
