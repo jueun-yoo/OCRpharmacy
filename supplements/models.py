b64584f875c6f78ec4c6b8fc7f1eb6769b287ccf
jueun <jeyoo7@naver.com>
2023-07-22 18:25:27 +0900

    model 연결

--- supplements/models.py
+++ supplements/models.py
@@ -1,30 +1,35 @@
 from django.db import models
 
-class Nutrient(models.Model):
+#영양소 1개 정보
+class Nutrient(models.Model): 
     name = models.CharField(max_length=200)
     details = models.TextField()
 
     def __str__(self):
         return self.name
 
+#영양소의 동의어 처리 - 하나의 영양소에 대해 여러개의 동의어 생성
 class Synonym(models.Model):
     nutrient = models.ForeignKey(Nutrient, related_name='synonyms', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    synonym = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
-    
+        return self.synonym
+
+#영양제 데이터, 영양제 이름과 여러개의 영양소 연결
 class Supplement(models.Model):
     name = models.CharField(max_length=200)
     nutrients = models.ManyToManyField(Nutrient, through='SupplementNutrient')
 
     def __str__(self):
         return self.name
-    
+
+#영양제와 영양소 데이터의 관계. "복용량" 데이터 추가를 위해서!!
 class SupplementNutrient(models.Model):
     nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
     supplement = models.ForeignKey(Supplement, on_delete=models.CASCADE)
     dosage = models.FloatField(default=0.0)
+
 
 class RecommendedNutrient(models.Model):
     gender = models.CharField(max_length=1, choices=[('M', '남성'), ('F', '여성')])
--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=100)
@@ -8,4 +8,20 @@ class User(AbstractUser):
     pregnant = models.BooleanField()
     breastfeeding = models.BooleanField()
 
-    recommended = models.ForeignKey('supplements.RecommendedNutrient', on_delete=models.SET_NULL, null=True, related_name='users',)
+    recommended = models.ForeignKey('supplements.RecommendedNutrient', on_delete=models.SET_NULL, null=True, related_name='users')
+
+class UserSupplement(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_supplements')
+    supplement = models.ForeignKey('supplements.Supplement', on_delete=models.CASCADE, related_name='user_supplements')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.supplement.name}'
+    
+class UserTotalIntake(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nutrient = models.ForeignKey('supplements.RecommendedNutrient', on_delete=models.CASCADE)
+    total_intake = models.FloatField(default=0.0)  # 누적 섭취량
+
+    def __str__(self):
+        return f'{self.user.username} - {self.total_intake}'
