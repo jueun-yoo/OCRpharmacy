2ca5cedff882dcc34820d73bc4ee479203430889
maenghyunii <note0315note@gmail.com>
2023-07-27 16:19:34 +0900

    ㅎ

--- supplements/admin.py
+++ supplements/admin.py
@@ -1,3 +1,10 @@
+# admin.py
 from django.contrib import admin
+from .models import Nutrient, Synonym, Supplement, SupplementNutrient, RecommendedIntake, RecommendedNutrient
 
-# Register your models here.
+admin.site.register(Nutrient)
+admin.site.register(Synonym)
+admin.site.register(Supplement)
+admin.site.register(SupplementNutrient)
+admin.site.register(RecommendedIntake)
+admin.site.register(RecommendedNutrient)
