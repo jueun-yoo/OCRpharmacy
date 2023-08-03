ab3a3709d4c7c3db2ba6176a19cb6f83b6a01551
maenghyunii <note0315note@gmail.com>
2023-08-03 18:08:23 +0900

    gg

--- user/views.py
+++ user/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+# user/views.py
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # 회원가입 후 로그인 페이지로 이동
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
