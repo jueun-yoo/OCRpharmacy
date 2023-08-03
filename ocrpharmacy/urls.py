2843e501dcd9e2c73e305112ce359bcbb1d0b5c8
maenghyunii <note0315note@gmail.com>
2023-08-03 18:47:14 +0900

    o

--- ocrpharmacy/urls.py
+++ ocrpharmacy/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('supplements/', include('supplements.urls')),
-    
 ]
