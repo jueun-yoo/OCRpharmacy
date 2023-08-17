2ae4371c1b1baf16fba0437caa3e0e8db7523ee4
maenghyunii <note0315note@gmail.com>
2023-08-17 15:20:49 +0900

    ..

--- supplements/OCR/naverapi.py
+++ supplements/OCR/naverapi.py
@@ -2,7 +2,14 @@ import uuid
 import json
 import time
 import requests
+import sqlite3
 from PIL import Image, ImageDraw, ImageFont
+
+#영양제,영양소,함량 추가 함수
+def add_dosage(dosage, nutrient_id, supplement_id):
+    sql = "INSERT INTO supplements_supplementnutrient (dosage, nutrient_id, supplement_id) VALUES (?, ?, ?)"
+    cursor.execute(sql, (dosage, nutrient_id, supplement_id))
+    conn.commit()
 
 api_url = 'https://3g69izliq5.apigw.ntruss.com/custom/v1/23692/83af5a7f71fe00ce5a40c5be9622db4c5114ba6702fdc14ddb1b4b007e5ca726/general'
 secret_key = 'UFRkQlFOSlhNUW5QS0FRamdtTnpOTWpjbmxGQ2JUek8='
@@ -53,68 +60,37 @@ if response.status_code == 200:
     extracted_text_str = ' '.join(extracted_text)  # 공백을 이용하여 하나의 줄로 합침
     extracted_text_str = extracted_text_str.replace(" ", "")  # Remove all spaces from the text
     print('추출된 텍스트:\n', extracted_text_str)  # 추출된 텍스트 확인
-    import re
+
     # 쉼표로 분리하여 리스트로 저장
     lines = extracted_text_str.split(',')
     print(lines)
-
-    nutrient_mapping = {
-        "열량": "열량",
-        "단백질": "단백질",
-        "식이섬유": "식이섬유",
-        "수분": "수분",
-        "비타민A": "비타민A",
-        "비타민D": "비타민D",
-        "비타민E": "비타민E",
-        "비타민K": "비타민K",
-        "비타민C": "비타민C",
-        "티아민": "티아민",
-        "리보플라민": "리보플라민",
-        "나이아신": "나이아신",
-        "비타민B6": "비타민B6",
-        "엽산": "엽산",
-        "비타민B12": "비타민B12",
-        "판토텐산": "판토텐산",
-        "비오틴": "비오틴",
-        "칼슘": "칼슘",
-        "인": "인",
-        "나트륨": "나트륨",
-        "염소": "염소",
-        "칼륨": "칼륨",
-        "마그네슘": "마그네슘",
-        "철": "철",
-        "아연": "아연",
-        "구리": "구리",
-        "불소": "불소",
-        "망간": "망간",
-        "요오드": "요오드",
-        "셀레늄": "셀레늄",
-        "몰데브덴": "몰데브덴"
-        # 필요한 영양소 매핑을 여기에 추가하세요
-        }
-
-    nutrient_data = {}
-
+    
+# SQLite 데이터베이스에 연결
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
+    # supplements_nutrient 테이블에서 모든 영양소 정보를 가져옴
+    cursor.execute('SELECT name FROM supplements_nutrient')
+    nutrient_data_db = cursor.fetchall()
+    # Extracted lines from OCR
     for line in lines:
-        for nutrient_name, nutrient_key in nutrient_mapping.items():
-            if nutrient_name in line:
-                if nutrient_name == "열량":
-                    value_start = line.find(nutrient_name) + len(nutrient_name)
+        for nutrient_name_db, in nutrient_data_db:  # 주목: 쉼표를 사용하여 영양소 이름을 튜플로 저장
+            if nutrient_name_db in line:
+                if nutrient_name_db == "열량":
+                    value_start = line.find(nutrient_name_db) + len(nutrient_name_db)
                     value_end = line.find("kcal", value_start) + len("kcal") + 1
                 else:
-                    value_start = line.find(nutrient_name) + len(nutrient_name)
+                    value_start = line.find(nutrient_name_db) + len(nutrient_name_db)
                     value_end = line.find("g", value_start) + 1
-                
-                nutrient_value = line[value_start:value_end].strip()  # Remove any extra spaces
-                nutrient_data[nutrient_key] = nutrient_value
-
-    print("영양소 데이터:", nutrient_data)
-
+                nutrient_value = line[value_start:value_end].strip()
+                add_dosage(nutrient_value, nutrient_name_db, 1)
+    # 연결 종료
+    conn.close()
+    
     # 이미지 저장
     output_image_file = r"C:\Microsoft VS Code\2023DNA\OCR\clova_image.jpg"
     original_image.save(output_image_file)
     print('\n이미지 저장 완료:', output_image_file)
-
+"""
     # 추출된 텍스트 저장
     output_text_file = r"C:\Microsoft VS Code\2023DNA\OCR\clova_text.txt"
     with open(output_text_file, 'w', encoding='utf-8') as f:
@@ -122,5 +98,4 @@ if response.status_code == 200:
         f.write(nutrient_data_str)
         print('텍스트 저장 완료:', output_text_file)
         print('저장된 텍스트:\n', nutrient_data_str)
-else:
-    print('API 요청에 실패했습니다. 상태 코드:', response.status_code)
+"""
