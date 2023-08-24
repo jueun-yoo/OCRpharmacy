f69c1800e67ee2c6d72830e711b508ef9dfc5804
maenghyunii <note0315note@gmail.com>
2023-08-24 23:28:20 +0900

    좀 꾸밈 ㅎ

--- supplements/views.py
+++ supplements/views.py
@@ -19,26 +19,13 @@ from django.urls import reverse
 from django.core.files.base import ContentFile
 import base64
 import numpy as np
+import random
 
 # Create your views here.
 # 영양소 상세 보기
 def nutrient_detail(request, nutrient_name):
     nutrient = get_object_or_404(Nutrient, name=nutrient_name)
     return render(request, 'supplements/nutrient_detail.html', {'nutrient': nutrient})
-"""
-# 영양소 상세 보기와 동의어 함께 보기
-def nutrient_detail_with_synonyms(request, nutrient_id):
-    nutrient = get_object_or_404(Nutrient, pk=nutrient_id)
-
-    # OCR에서 가져온 영양소 표현 (예시로 "비타민B1"로 가정)
-    ocr_expression = "비타민B1"
-
-    # OCR로부터 가져온 표현이 "비타민B1"인 경우, "티아민"으로 변경
-    if ocr_expression == "비타민B1":
-        ocr_expression = "티아민"
-
-    return render(request, 'nutrient_detail_with_synonyms.html', {'nutrient': nutrient, 'ocr_expression': ocr_expression})
- """
 
 def supplement_detail(request, supplement_id):
     supplement = get_object_or_404(Supplement, id=supplement_id)
